eca94fa98f534a00e0cf3f4cc4586d98bf776296
Saul SM <saul.salazar.mendez@gmail.com>
2021-08-15 13:35:18 -0500

    UPDATE - se actualiza el ejemplo

--- js/prueba.js
+++ js/prueba.js
@@ -2,53 +2,29 @@ import './editor/editor-archivos.js';
 
 let editor = document.querySelector('editor-archivos-codejar');
 
-const my = editor => {
-    let code = editor.textContent;
-    code = code.replace(
-      /\((\w+?)(\b)/g,
-      '(<font color="#8a2be2">$1</font>$2'
-    );
-    editor.innerHTML = code;
-};
-
-
-const rainbow = (ch, i) => {
-    let red   = Math.round(Math.sin(0.01 * i + 0) * 127 + 128);
-    let green = Math.round(Math.sin(0.01 * i + 2 * Math.PI / 3) * 127 + 128);
-    let blue  = Math.round(Math.sin(0.01 * i + 4 * Math.PI / 3) * 127 + 128);
-    return `<span style="color: rgb(${red}, ${green}, ${blue})">${ch}</span>`;
-  };
-  
-  const lolcat = editor => {
-    const code = editor.textContent
-      .split('')
-      .map(rainbow)
-      .join('');
-    editor.innerHTML = code;
-  };
-  
-
 editor.setFiles([
     {
         titulo: 'HTML',
         tipo: 'html',
-        codigo: '<!--HTML-->'
+        codigo: '<!--HTML-->',
+        icono:`&#9733;`
     },
     {
         titulo: 'CSS',
         tipo: 'css',
-        codigo: '/*css*/'
+        codigo: '/*css*/',
+        icono: '&#9733;'
     },
     {
         titulo: 'Js',
         tipo: 'js',
         codigo: '//js',
-        fcolor: lolcat
+        icono: '&#9733;'
     },
     {
-        titulo: 'YOLO',
-        tipo: 'yolo',
-        codigo: '/*yolo*/',
-        fcolor: my
+        titulo: 'Python',
+        tipo: 'python',
+        codigo: '',
+        icono: '&#9733;'
     }
 ]);
